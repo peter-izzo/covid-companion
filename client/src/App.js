033ea4217e584ced8992e1e896831982a469db22
rpc08002 <rcurtin0491@gmail.com>
2021-04-06 18:32:48 -0400

    added concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import React from "react";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { Auth } from "aws-amplify";
 
-const App = () => (
-  <div>
-    <AmplifySignOut />
-    My App
-  </div>
-);
+const App = () => {
+  const handleClick = async() => {
+    console.log('click');
+    const results = await Auth.currentAuthenticatedUser();
+    console.log(results.attributes.sub);
+  }
+  return (
+    <div>
+      <AmplifySignOut />
+      My App <button onClick={handleClick}>Submit</button>
+    </div>
+  );
+};
 
 export default withAuthenticator(App);
