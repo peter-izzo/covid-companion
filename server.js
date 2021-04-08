d3fc114f89e6298e558c131cd05d8fced3dec43c
Danny Smooke <dana.smooke@gmail.com>
2021-04-08 19:03:55 -0400

    edited connection url - review

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
-import express from "express";
-import mongoose from "mongoose";
+const express = require("express");
+const mongoose = require("mongoose");
 
 // App Config
+const PORT = process.env.PORT || 3000;
 const app = express();
-const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// DB config //@audit mongo link and connection_url
 const connection_url =
   "mongodb+srv://admin:13umYzqHo5zsxCJF@cluster0.vzmrq.mongodb.net/covidcompaniondb?retryWrites=true&w=majority";
 
-// Middlewares
-
-// DB config
-mongoose.connect(connection_url, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(
+    process.env.MONGODB_URI ||
+      "mongodb://localhost/covidcompaniondb" ||
+      connection_url,
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("   *** Connected to Database ***"))
+  .catch((err) => console.log(err));
 
 // API Endpoints
 app.get("/", (req, res) => res.status(200).send("Hello World!"));
