0afe1849afff02b2b6e2c94825beaed603727951
Julie Ann <julieanniwinski@gmail.com>
2021-04-08 20:27:18 -0400

    fixed server.js issue

--- server.js
+++ server.js
@@ -1,33 +1,13 @@
-<<<<<<< HEAD
-const express = require("express");
-const mongoose = require("mongoose");
-=======
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
->>>>>>> 7b5c7c939d67fb61b81664b802c6f3848edad52d
 
 // App Config
 const PORT = process.env.PORT || 3000;
 const app = express();
-<<<<<<< HEAD
-
-// Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-// DB config //@audit mongo link and connection_url
-const connection_url =
-  "mongodb+srv://admin:13umYzqHo5zsxCJF@cluster0.vzmrq.mongodb.net/covidcompaniondb?retryWrites=true&w=majority";
-=======
 const port = process.env.PORT || 3000;
 const connection_url = process.env.MONGODB_STRING;
->>>>>>> 7b5c7c939d67fb61b81664b802c6f3848edad52d
 
 mongoose
   .connect(
