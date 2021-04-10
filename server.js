43dcb9dc078fa6c47f6a908f060b72e9de2d1219
Cris Franco <cris.franco219@gmail.com>
2021-04-10 13:27:35 -0400

    finished setting up schema

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
-
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
-import quesSchema from "./models/quesSchema";
+import { User } from "./models/userModel.js";
 
 // App Config
-const PORT = process.env.PORT || 3000;
 const app = express();
-
-
-// Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-// DB config //@audit mongo link and connection_url
-const connection_url =
-  "mongodb+srv://admin:13umYzqHo5zsxCJF@cluster0.vzmrq.mongodb.net/covidcompaniondb?retryWrites=true&w=majority";
-
 const port = process.env.PORT || 3000;
 const connection_url = process.env.MONGODB_STRING;
 
