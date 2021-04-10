8415f7d34e859b9a791b0a3807c22d46f4f980db
Peter Izzo <peter.izzo528@gmail.com>
2021-04-10 10:32:13 -0400

    idk if this'll work

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,6 @@ import SignUp from "./pages/SignUp";
 import Questionnaire from "./pages/Questionnaire";
 import Profile from "./pages/Profile";
 import SearchFriends from "./pages/SearchFriends";
-import Amplify, { Auth } from 'aws-amplify';
 
 const App = () => {
   const handleClick = async () => {
