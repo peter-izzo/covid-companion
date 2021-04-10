bb2280687772a6e8b0f71be323912e4082d87855
Peter Izzo <peter.izzo528@gmail.com>
2021-04-10 10:33:10 -0400

    m

--- server.js
+++ server.js
@@ -1,34 +1,25 @@
-<<<<<<< HEAD
-const express = require("express");
-const mongoose = require("mongoose");
-=======
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
->>>>>>> 7b5c7c939d67fb61b81664b802c6f3848edad52d
-
+import quesSchema from "./models/quesSchema";
+​
 // App Config
 const PORT = process.env.PORT || 3000;
 const app = express();
-<<<<<<< HEAD
-
-// Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
+​
 // DB config //@audit mongo link and connection_url
 const connection_url =
   "mongodb+srv://admin:13umYzqHo5zsxCJF@cluster0.vzmrq.mongodb.net/covidcompaniondb?retryWrites=true&w=majority";
-=======
+​
 const port = process.env.PORT || 3000;
 const connection_url = process.env.MONGODB_STRING;
->>>>>>> 7b5c7c939d67fb61b81664b802c6f3848edad52d
-
+​
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+​
+// Connection to MongoDB
 mongoose
   .connect(
     process.env.MONGODB_URI ||
@@ -42,11 +33,12 @@ mongoose
   )
   .then(() => console.log("   *** Connected to Database ***"))
   .catch((err) => console.log(err));
-
+​
 // API Endpoints
 app.get("/", (req, res) => res.status(200).send("Hello World!"));
-
+​
 // Need a post req to pass in data to the db //
-
+app.post("/questions/post", (req, res) => {});
+​
 // Listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
