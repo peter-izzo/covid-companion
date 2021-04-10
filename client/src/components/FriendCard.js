46f48d28ee1d7b836287078f14f85c4350d814a2
Danny Smooke <dana.smooke@gmail.com>
2021-04-10 09:37:40 -0400

    accepted both changes for edited files

--- client/src/components/FriendCard.js
+++ client/src/components/FriendCard.js
@@ -2,7 +2,6 @@ import React, { useReducer } from "react";
 import CardBtn from "./CardBtn";
 
 function FriendCard() {
-<<<<<<< HEAD
   {
     /* @todo add loop to fill in cards with names*/
   }
@@ -33,24 +32,24 @@ function FriendCard() {
 }
 
 export default FriendCard;
-=======
 
-    return(
-        <main className="mw6 center">
-            <article className="dt w-100 bb b--black-05 pb2 mt2" href="#0">
-                <div className="dtc w2 w3-ns v-mid">
-                    <img src={user.image} className="ba b--black-10 db br-100 w2 w3-ns h2 h3-ns" alt={user._id}/>
-                </div>
-                <div class="dtc v-mid pl3">
-                    <h1 class="f6 f5-ns fw6 lh-title black mv0">{user.name}</h1>
-                    <h2 class="f6 fw4 mt0 mb0 black-60">{user.username}</h2>
-                </div>
-                <CardBtn />
-            </article>
-        </main>
-    );
-};
-
+return (
+  <main className="mw6 center">
+    <article className="dt w-100 bb b--black-05 pb2 mt2" href="#0">
+      <div className="dtc w2 w3-ns v-mid">
+        <img
+          src={user.image}
+          className="ba b--black-10 db br-100 w2 w3-ns h2 h3-ns"
+          alt={user._id}
+        />
+      </div>
+      <div class="dtc v-mid pl3">
+        <h1 class="f6 f5-ns fw6 lh-title black mv0">{user.name}</h1>
+        <h2 class="f6 fw4 mt0 mb0 black-60">{user.username}</h2>
+      </div>
+      <CardBtn />
+    </article>
+  </main>
+);
 
 export default FriendCard;
->>>>>>> 0afe1849afff02b2b6e2c94825beaed603727951
