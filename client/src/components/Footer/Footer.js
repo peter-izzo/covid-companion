951a7466dd8350c8999bb37f69fe3529a14547d9
Danny Smooke <dana.smooke@gmail.com>
2021-04-10 13:59:59 -0400

    integrated Footer into profile

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,34 +1,51 @@
 import React from "react";
-import "./footer.css/"
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import "./footer.css";
 
 function Footer() {
+  const location = useLocation();
+  return (
+    <footer className="pb5 ml3 mr3">
+      <small className="f6 db tc">
+        © 2021 <b className="ttu">COVID Companion</b>., All Rights Reserved
+      </small>
 
-    return (
-        <footer>
-            <small class="f6 db tc">© 2021 <b class="ttu">COVID Companion</b>., All Rights Reserved</small>
+      <ul className="Horizontal-List">
+        <li className="Horizontal-List-Item">
+          <Link
+            to="../index.js/"
+            className={
+              (location.pathname === "/" ? "nav-link active" : "nav-link", "f7")
+            }
+          >
+            Language
+          </Link>
+        </li>
 
-            <ul className ="Horizontal-List">
-                <li className="Horizontal-List-Item">
-                    <Link to="../index.js/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-                        Language
-                    </Link>
-                </li>
+        <li className="Horizontal-List-Item">
+          <Link
+            to="/"
+            className={
+              (location.pathname === "/" ? "nav-link active" : "nav-link", "f7")
+            }
+          >
+            Terms of Use
+          </Link>
+        </li>
 
-                <li className="Horizontal-List-Item">
-                    <Link to="/" classname={location.pathname==="/" ? "nav-link active" : "nav-link"}>
-                        Terms of Use 
-                    </Link>
-                </li>
-
-                <li className="Horizontal-List-Item">
-                    <Link to="/" classname={location.pathname==="/" ? "nav-link active" : "nav-link"}>
-                        Privacy 
-                    </Link>
-                </li>
-            </ul>
-        </footer>
-    );
-};
+        <li className="Horizontal-List-Item">
+          <Link
+            to="/"
+            className={
+              (location.pathname === "/" ? "nav-link active" : "nav-link", "f7")
+            }
+          >
+            Privacy
+          </Link>
+        </li>
+      </ul>
+    </footer>
+  );
+}
 
 export default Footer;
