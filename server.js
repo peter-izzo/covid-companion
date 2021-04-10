e3bbe0c80016026657f2a8df2427eba06c26d71d
Julie Ann <julieanniwinski@gmail.com>
2021-04-10 11:11:39 -0400

    Cris changes to server.js

--- server.js
+++ server.js
@@ -2,16 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
-import quesSchema from "./models/quesSchema";
+//import quesSchema from "./models/quesSchema";
 ​
 // App Config
-const PORT = process.env.PORT || 3000;
 const app = express();
-​
-// DB config //@audit mongo link and connection_url
-const connection_url =
-  "mongodb+srv://admin:13umYzqHo5zsxCJF@cluster0.vzmrq.mongodb.net/covidcompaniondb?retryWrites=true&w=majority";
-​
 const port = process.env.PORT || 3000;
 const connection_url = process.env.MONGODB_STRING;
 ​
@@ -38,7 +32,6 @@ mongoose
 app.get("/", (req, res) => res.status(200).send("Hello World!"));
 ​
 // Need a post req to pass in data to the db //
-app.post("/questions/post", (req, res) => {});
 ​
 // Listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
