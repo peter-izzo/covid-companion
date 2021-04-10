bc4a6b54ee4b028e8c8d20e8cf8a211a066c7e11
Bruno Borges <brunobborges68@gmail.com>
2021-04-10 10:29:26 -0400

    fixing errors

--- client/src/components/FriendCard.js
+++ client/src/components/FriendCard.js
@@ -22,8 +22,7 @@ function FriendCard() {
         <form class="w-100 tr">
           <button
             class="f6 button-reset bg-white ba b--black-10 dim pointer pv1 black-60"
-            type="submit"
-          >
+            type="submit">
             + Follow
           </button>
         </form>
--- server.js
+++ server.js
@@ -1,36 +1,24 @@
-
-const express = require("express");
-const mongoose = require("mongoose");
-
-import dotenv from "dotenv";
+​import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import quesSchema from "./models/quesSchema";
-
+​
 // App Config
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-// Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
 // DB config //@audit mongo link and connection_url
 const connection_url =
   "mongodb+srv://admin:13umYzqHo5zsxCJF@cluster0.vzmrq.mongodb.net/covidcompaniondb?retryWrites=true&w=majority";
-
+​
 const port = process.env.PORT || 3000;
 const connection_url = process.env.MONGODB_STRING;
-
+​
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+​
 // Connection to MongoDB
 mongoose
   .connect(
@@ -45,12 +33,12 @@ mongoose
   )
   .then(() => console.log("   *** Connected to Database ***"))
   .catch((err) => console.log(err));
-
+​
 // API Endpoints
 app.get("/", (req, res) => res.status(200).send("Hello World!"));
-
+​
 // Need a post req to pass in data to the db //
 app.post("/questions/post", (req, res) => {});
-
+​
 // Listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
