860ea41fd2afd5b5ba8c52c8096b654d95565742
Peter Izzo <peter.izzo528@gmail.com>
2021-04-13 19:12:39 -0400

    push to pull

--- server.js
+++ server.js
@@ -4,34 +4,34 @@ import express from "express";
 import mongoose from "mongoose";
 import { User } from "./models/userModel.js";
 
-// App Config
+//App Config
 const app = express();
 const port = process.env.PORT || 3000;
 const connection_url = process.env.MONGODB_STRING;
-​
-// Middlewares
+
+//Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-​
-// Connection to MongoDB
+
+//Connection to MongoDB
 mongoose
   .connect(
     process.env.MONGODB_URI ||
       "mongodb://localhost/covidcompaniondb" ||
       connection_url,
     {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
     }
   )
   .then(() => console.log("   *** Connected to Database ***"))
-  .catch((err) => console.log(err));
-​
+  .catch((err) => console.log(err))
+
 // API Endpoints
-app.get("/", (req, res) => res.status(200).send("Hello World!"));
-​
-// Need a post req to pass in data to the db //
-​
-// Listener
+app.get("/", (req,res) => res.status(200).send("Hello World!"));
+
+// Need a post req to pass in data to the db
+
+//Listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
