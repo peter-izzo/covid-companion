30b4d15d8bbcdff5801cade9c87eeb1f47b4e1f0
Danny Smooke <dana.smooke@gmail.com>
2021-04-17 12:57:25 -0400

    merging after ryan's questionnaire change

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,20 +1,16 @@
-<<<<<<< HEAD
-import React from "react";
-=======
 // import AvatarBio, CovidInfo, FriendList (=FriendCard x5)
 
 import React, { useEffect, useState } from "react";
->>>>>>> 8822984a855b1d83ff7277724bfc7a3479dc9491
 import AvatarBio from "../components/AvatarBio";
 import FriendCircle from "../components/FriendCircle";
 // import CovidInfo from "../components/CovidInfo";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import { Auth } from "aws-amplify";
-import axios from "axios"
+import axios from "axios";
 
 function Profile() {
-  const [userData, setUserData] = useState()
+  const [userData, setUserData] = useState();
   async function getUserData() {
     const user = await Auth.currentAuthenticatedUser();
     axios.get(`http://localhost:3001/profile/${user.attributes.sub}`).then(
@@ -37,13 +33,8 @@ function Profile() {
         <article className="mw6 center bg-white br3 pa4-ns mv3 ba b--black-10 pb7 test">
           <div className="tc">
             <div className="test2">
-<<<<<<< HEAD
-              <AvatarBio />
-              {/* <CovidInfo /> */}
-=======
               <AvatarBio user={userData} />
               <CovidInfo user={userData} />
->>>>>>> 8822984a855b1d83ff7277724bfc7a3479dc9491
             </div>
             <FriendCircle />
           </div>
